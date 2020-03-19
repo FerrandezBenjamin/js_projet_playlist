ff7bed89c33623823ed76498688863de5aa25d8d
FerrandezBenjamin <58741067+FerrandezBenjamin@users.noreply.github.com>
2020-03-19 15:17:21 +0100

    Update script.js

--- 1-decouvrir-DOM-jquery/niveau1/script.js
+++ 1-decouvrir-DOM-jquery/niveau1/script.js
@@ -1,1 +1,29 @@
+$filmList = $.get('./playlist.txt', function(data)
+{
+    var datasplited = (data.split('\n'));
+    $(document).ready(function()
+    {
+        $('button').click(function()
+        {
+                $("li").each(function()
+                {
+                    console.log(datasplited);
+                });
+        });
+    });
+});
 
+
+
+// $filmList = $.get('./playlist.txt', function(data)
+// {
+//     var datasplited = (data.split('\n'));
+//     $(document).ready(function()
+//     {
+//     $('button').click(function()
+//         {
+//             console.log(datasplited);
+//         });
+    
+//     });
+// });
